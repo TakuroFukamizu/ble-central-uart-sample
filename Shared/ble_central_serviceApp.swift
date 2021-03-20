d843d6239eb7b76ed7bfb420f48289394429700d
Takuro Fukamizu <takuro.f.1987@gmail.com>
2021-03-20 20:37:43 +0900

    Initial Commit

---
+++ Shared/ble_central_serviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_central_serviceApp.swift
+//  Shared
+//
+//  Created by TAKURO FUKAMIZU on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ble_central_serviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
